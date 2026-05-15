89a1015eb5d382bde123e4a88bcb3427530abf20
kartikeyaanumanla-cpu <kartikeyaanumanla@gmail.com>
2026-05-15 15:14:57 +0530

    model trainig is done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,8 @@ from src.logger import logging
 import pandas as pd
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
@@ -61,6 +63,8 @@ if __name__=="__main__":
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformer(train_data,test_data)
+    
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
-
-
+ 
